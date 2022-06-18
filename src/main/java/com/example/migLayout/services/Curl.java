a74eb9a4d7a86486d9e0552d4e2357b6b7351bdf
vosmerkin <vosmerkin.evgen@gmail.com>
2022-06-18 17:46:06 +0000

    curlTest1

--- src/main/java/com/example/migLayout/services/Curl.java
+++ src/main/java/com/example/migLayout/services/Curl.java
@@ -7,6 +7,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
--- src/test/java/com/example/migLayout/services/CurlTest.java
+++ src/test/java/com/example/migLayout/services/CurlTest.java
@@ -1,28 +1,46 @@
 package com.example.migLayout.services;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurlTest {
+    private MockWebServer mockWebServer;
     Curl curl;
-    Curl.Builder curlBuilder;
+    Map<String, String> map;
+    String data;
 
     @BeforeEach
     void setUp() {
-        String result = new Curl.Builder(Adresses.CREATE)
+        this.mockWebServer = new MockWebServer();
+        map = new HashMap<>();
+        map.put("Content-Type", "application/json");
+
+
+        curl = new Curl.Builder(mockWebServer.url("/").toString())
                 .method(Curl.HttpMethod.POST)
                 .headers(map)
                 .data(data)
-                .create()
-                .call();
+                .create();
+
     }
 
     @Test
     @DisplayName("Simple multiplication should work")
     void testRequest() {
+        mockWebServer.enqueue(new MockResponse()
+                .addHeader("Content-Type", "application/json; charset=utf-8")
+                .setResponseCode(500));
+        String result = curl.call();
 
+        assertEquals("Lorem ipsum dolor sit amet.", result);
     }
 }
